d5a08e0dd7b061b5e15fab8f4428e8672f408369
herm23 <serainhermann01@gmail.com>
2025-11-22 15:06:26 +0100

    step 3 attiva navigation

--- controller/src/navigation_node.cpp
+++ controller/src/navigation_node.cpp
@@ -27,7 +27,7 @@ public:
   {
       // SERVICE CLIENT PER ATTIVARE LOCALIZATION
       localization_client = this->create_client<nav2_msgs::srv::ManageLifecycleNodes>("/lifecycle_manager_localization/manage_nodes");
-      //navigation_client = this->create_client<nav2_msgs::srv::ManageLifecycleNodes>("/lifecycle_manager_navigation/manage_nodes");
+      navigation_client = this->create_client<nav2_msgs::srv::ManageLifecycleNodes>("/lifecycle_manager_navigation/manage_nodes");
       RCLCPP_INFO(this->get_logger(), "localization client init\n");
 
       // PUBLISH DELLA POSIZIONE INIZIALE
@@ -37,36 +37,19 @@ public:
 
   }
 
-
   rclcpp::Client<ServiceT>::SharedFuture send_localization_request()
   {
-    auto request = std::make_shared<ServiceT::Request>();
-    request->command = ServiceT::Request::STARTUP;
-
-    // aspetta il service
-    while (!localization_client->wait_for_service(1s)) {
-      
-      if (!rclcpp::ok()) {
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),"Interrupted by the user. Exit!");
-        return rclcpp::Client<ServiceT>::SharedFuture{};
-      }
-
-      RCLCPP_INFO(
-        rclcpp::get_logger("rclcpp"),
-        "Service /lifecycle_manager_localization/manage_nodes not available, waiting again...");
-    }
-
-    RCLCPP_INFO(
-      this->get_logger(),
-      "Sending localization lifecycle req. | command: %u",
-      static_cast<unsigned>(request->command));
-
-    auto result = localization_client->async_send_request(request);
-    return result;
+    return send_client_request(true);
   }
 
+  rclcpp::Client<ServiceT>::SharedFuture send_navigation_request()
+  {
+    return send_client_request(false);
+  }
+  
+
   //NOTE: Gestione univoca della feature per richieste asincrone
-  void handle_navigation_result(rclcpp::Client<ServiceT>::SharedFuture result) {
+  void handle_result(rclcpp::Client<ServiceT>::SharedFuture result, std::string serviceName) {
     if (!result.valid()) {
       rclcpp::shutdown();
       return;
@@ -74,23 +57,14 @@ public:
 
     auto self = this->shared_from_this();
 
-
     if (rclcpp::spin_until_future_complete(self, result) == rclcpp::FutureReturnCode::SUCCESS)
     {
       auto response = result.get();
-
-      // per ManageLifecycleNodes il campo è "success", non "response"
-      RCLCPP_INFO(
-        this->get_logger(),
-        "Response: %s",
-        response->success ? "true" : "false"
-      );      
+      RCLCPP_INFO(this->get_logger(),"Response: %s",response->success ? "true" : "false");      
     }
     else
     {
-      RCLCPP_ERROR(
-        this->get_logger(),
-        "Failed to call service /lifecycle_manager_navigation/manage_nodes");
+      RCLCPP_ERROR(this->get_logger(), "Failed to call service %s", serviceName.c_str());
     }
   }
 
@@ -98,15 +72,53 @@ private:
   //Class Members
   rclcpp::Client<nav2_msgs::srv::ManageLifecycleNodes>::SharedPtr localization_client;
   rclcpp::Client<nav2_msgs::srv::ManageLifecycleNodes>::SharedPtr navigation_client;
+
+  rclcpp::Client<ServiceT>::SharedFuture send_client_request(bool isLocalization)
+  {
+    auto request = std::make_shared<ServiceT::Request>();
+    request->command = ServiceT::Request::STARTUP;
+
+    auto client = isLocalization ? localization_client : navigation_client;
+    std::string serviceName = isLocalization ? "/lifecycle_manager_localization/manage_nodes" : "/lifecycle_manager_navigation/manage_nodes";
+
+    // aspetta il service
+    while (!client->wait_for_service(1s)) {
+      
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),"Interrupted by the user. Exit!");
+        return rclcpp::Client<ServiceT>::SharedFuture{};
+      }
+
+      RCLCPP_INFO( rclcpp::get_logger("rclcpp"), "Service %s not available, waiting again...", serviceName.c_str());
+    }
+
+    RCLCPP_INFO(
+      this->get_logger(),
+      "Sending localization lifecycle req. | command: %u",
+      static_cast<unsigned>(request->command));
+
+    auto result = client->async_send_request(request);
+    return result;
+  }
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   std::shared_ptr<NavigationNode> nav_node = std::make_shared<NavigationNode>();
-  auto result = nav_node->send_localization_request();
+  
+  //STEP 1
+  auto result_localization = nav_node->send_localization_request();
+  nav_node->handle_result(result_localization, "/lifecycle_manager_localization/manage_nodes");
 
-  nav_node->handle_navigation_result(result);
+  //STEP 2
+
+  //STEP 3
+  auto result_nav = nav_node->send_navigation_request();
+  nav_node->handle_result(result_nav, "/lifecycle_manager_navigation/manage_nodes");
+
+  //STEP 4
+
 
   rclcpp::shutdown();
   return 0;
